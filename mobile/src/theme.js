1ec4808de1b84f515c204079177208e0d3027b4b
Renoir Auguste S.S <148928847+RenoirAugusteSS@users.noreply.github.com>
2026-06-17 21:16:09 -0300

    feat: add mobile/src/theme.js

---
+++ mobile/src/theme.js
@@ -0,0 +1,23 @@
+export const colors = {
+  verde: '#0F6E56',
+  verdeClaro: '#E1F5EE',
+  verdeBorda: '#9FE1CB',
+  azul: '#185FA5',
+  ambar: '#854F0B',
+  ambarClaro: '#FAEEDA',
+  vermelho: '#A32D2D',
+  vermelhoClaro: '#FCEBEB',
+  textoPrincipal: '#2C2C2A',
+  textoSecundario: '#5F5E5A',
+  borda: '#D3D1C7',
+  fundo: '#F1EFE8',
+  branco: '#FFFFFF',
+};
+
+export const spacing = {
+  xs: 4,
+  sm: 8,
+  md: 16,
+  lg: 24,
+  xl: 32,
+};
